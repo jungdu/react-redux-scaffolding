99719bc828cb29c334dd8b2450e7a04e413df56a
jungdu <jungdujang@gmail.com>
2019-03-14 19:27:42 +0900

    styled-component 라이브러리추가 & App 컴포넌트 스타일 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: white;
+  background-color: black;
+`;
 
 class App extends Component {
   render() {
-    return <div className="App">App</div>;
+    return <AppWrapper>App</AppWrapper>;
   }
 }
 
